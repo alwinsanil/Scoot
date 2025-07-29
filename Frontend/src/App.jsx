ddf4d1944e2c918e02880ed31b13001280494ebe
Alwin Sanil <alwinsanil@gmail.com>
2025-07-28 23:22:51 -0300

    refactor: remove FeedbackModalDisplay component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Cipher from './pages/auth/Cipher';
 import QnA from './pages/auth/QnA';
 import Owner from './pages/Owner';
 import User from './pages/User';
-import FeedbackSentimentDisplay from './pages/Feedback';
 import AnalyticsDashboard from './pages/AnalyticsDashboard';
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route path="/auth" element={<Auth />} />
         <Route path="/auth/qna/callback" element={<QnA />} />
         <Route path="/owner" element={<Owner />} />
-        <Route path="/feedback" element={<FeedbackSentimentDisplay />} />
         <Route path="/auth/cipher/callback" element={<Cipher />} />
         <Route path="/analytics" element={<AnalyticsDashboard />} />
       </Routes>
